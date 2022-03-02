9189b21070c41eb59dfc387c343cdf35fc1859d6
Kenneth Wu <kunhan419.wu@gmail.com>
2022-03-03 04:29:28 +0800

    postgresql schema update

--- db/qna/schema.sql
+++ db/qna/schema.sql
@@ -5,13 +5,13 @@ CREATE DATABASE QnA;
 \c QnA
 
 CREATE TABLE IF NOT EXISTS product (
- product_id SERIAL PRIMARY KEY
+ product_id BIGSERIAL
 );
 
 ALTER TABLE product ADD CONSTRAINT product_pkey PRIMARY KEY (product_id);
 
 CREATE TABLE questions (
- question_id SERIAL,
+ question_id BIGSERIAL,
  question_body VARCHAR(100),
  question_date VARCHAR(50),
  asker_name VARCHAR(20),
@@ -25,7 +25,7 @@ ALTER TABLE questions ADD CONSTRAINT questions_pkey PRIMARY KEY (question_id);
 -- ALTER TABLE questions ADD CONSTRAINT questions_pkey KEY ();
 
 CREATE TABLE answers (
- answer_id SERIAL,
+ answer_id BIGSERIAL,
  answer_body VARCHAR,
  answer_date VARCHAR,
  answerer_name VARCHAR,
